2e8ed6da091d322f2b49a379b8ae59722ddd392e
luwei <1280380446@qq.com>
2026-08-04 16:53:36 +0800

    fix: type check Python 3.8 timezone fallback

--- src/blrec/compat.py
+++ src/blrec/compat.py
@@ -1,8 +1,10 @@
 from __future__ import annotations
 
-try:
+import sys
+
+if sys.version_info >= (3, 9):
     from zoneinfo import ZoneInfo
-except ImportError:  # pragma: no cover - exercised by the Python 3.8 CI job
-    from backports.zoneinfo import ZoneInfo  # type: ignore
+else:  # pragma: no cover - exercised by the Python 3.8 CI job
+    from backports.zoneinfo import ZoneInfo
 
 __all__ = ['ZoneInfo']
