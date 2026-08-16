d2ecbf068a66486fff561f12b703c53ff1d88714
luwei <1280380446@qq.com>
2026-08-16 22:42:08 +0800

    release: prepare server 3.0.0-beta.93

--- src/blrec/__init__.py
+++ src/blrec/__init__.py
@@ -1,3 +1,3 @@
 __prog__ = 'blrec'
-__version__ = '3.0.0-beta.92'
+__version__ = '3.0.0-beta.93'
 __github__ = 'https://github.com/acgnhiki/blrec'
--- tests/release/test_version_metadata.py
+++ tests/release/test_version_metadata.py
@@ -6,7 +6,7 @@ ROOT = Path(__file__).resolve().parents[2]
 
 
 def test_release_version_matches_preview_beta() -> None:
-    assert blrec.__version__ == '3.0.0-beta.92'
+    assert blrec.__version__ == '3.0.0-beta.93'
 
 
 def test_server_and_worker_runtime_pins_cover_supported_python_wheels() -> None:
